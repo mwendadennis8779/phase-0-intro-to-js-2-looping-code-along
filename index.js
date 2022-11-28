dc9d3f068bdd07837f4e71bc720d907c35bf09b5
Dennis <mwendadennis8779@gmail.com>
2022-11-28 10:38:25 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+// Code your solutions in this fileconst name= [
+    
+    let name = ["Guadalupe", "Ollie", "Aki"]
+function writeCards(name, event) {
+    
+    for(let i = 0; i < name.length; i++){
+        return name.map(name => `Thank you, ${name}, for the wonderful ${event} gift!`)
+
+    }
+}
+function countDown(number){
+    while(number > 0){
+        console.log(number);
+        number -= 1;
+    }
+    console.log(number)
+
+
+}
+   
+
+
